c17ead5d8be05ffd430fb51014a1545906f80082
Onyeka Obi <Onyeka.Obi@gmail.com>
2017-04-24 22:19:06 -0700

    Use the thread pool, son!

--- Foreach.h
+++ Foreach.h
@@ -2,16 +2,16 @@
 #include <process.h>
 
 template<typename ActionType>
-unsigned Launch(void * context)
+void Launch(PTP_CALLBACK_INSTANCE instance, void * context, PTP_WORK work)
 {
-	(* (ActionType *)context)();
-	return 0;
+	(* (ActionType *)(context))();
 }
 
 template<typename ActionType, unsigned NumberOfActions>
 void ForEach(ActionType actions[], unsigned timeout)
 {
-	void * references[NumberOfActions];
-	for (unsigned index = 0; index < NumberOfActions; ++index) references[index] = ((void *)_beginthreadex(nullptr, 0, Launch<ActionType>, actions + index, 0, nullptr));
-	for (unsigned index = 0; index < NumberOfActions; ++index) WaitForSingleObjectEx(references[index], timeout, 0);
+	PTP_WORK workItems[NumberOfActions];
+	for (unsigned index = 0; index < NumberOfActions; ++index) workItems[index] = CreateThreadpoolWork(Launch<ActionType>, actions + index, nullptr);
+	for (unsigned index = 0; index < NumberOfActions; ++index) SubmitThreadpoolWork(workItems[index]);
+	for (unsigned index = 0; index < NumberOfActions; ++index) WaitForThreadpoolWorkCallbacks(workItems[index], false);
 }
